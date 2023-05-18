a3d96cd9f4b99c09b6b4f51302cb2e773f288200
Agota Hanasz <agota.hanasz.1990@icloud.com>
2023-05-18 21:21:07 +0200

    .

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const API_KEY = '1Pj3D3xLuzlgf4xeuzJk28OfHUqQwe0LksLWgJLN'
 let API_DATE = '2023-02-28'
 const rootElement = document.querySelector('#root')
 
+let div1
+let div2
+
 const fetchApod = async function(url) {
   const response = await fetch(url)
   const apodData = await response.json() 
@@ -12,10 +15,10 @@ const fetchApod = async function(url) {
 // statikus soha nem lesz cserélve (dátumválasztó)
 const createSkeleton = function() {
   
-  const div1 = document.createElement('div')
+  div1 = document.createElement('div')
   div1.classList.add('input-container')
 
-  const div2 = document.createElement('div')
+  div2 = document.createElement('div')
   div2.classList.add('data-container')
   rootElement.append(div1, div2)
 }
@@ -25,24 +28,21 @@ const createApodElement = function(parentElement, data) {
   //parentElement.innerHTML = '' (az elem önmagát fűzi hozzá) 
   const date = document.createElement('date')
   date.innerText = data.date
-  date.append(div1)
-
-  const div2 = document.createElement('div')
-  div2.classList.add('data-container')
+  div1.append(date)
 
   const h1 = document.createElement('h1')
   h1.innerText = data.title
-  h1.append(div2)
+  div2.append(h1)
 
   const explanation = document.createElement('explanation')
   explanation.innerText = data.explanation 
-  explanation.append(div2)
+  div2.append(explanation)
 
   const img = document.createElement('img')
   img.src = data.url
-  img.append(div2)
+  div2.append(img)
 
-  parentElement.append(date, explanation, h1, img)
+  parentElement.append(h1, explanation, img)
 }
 
 const init = async function() {
@@ -50,9 +50,10 @@ const init = async function() {
   const inputContainer = document.querySelector('.input-container')
   const dataContainer = document.querySelector('.data-container')
   const apodData = await fetchApod(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${API_DATE}`)
-  console.log(apodData)
+  console.info(apodData)
   createApodElement(dataContainer, apodData)
-  
+  console.info(div1, div2)
+
 }
 
 init()
