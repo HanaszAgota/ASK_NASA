799aea32a7cd55491bf0beca43778670c38d06bc
Agota Hanasz <agota.hanasz.1990@icloud.com>
2023-05-18 16:37:42 +0200

    addeed async

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_KEY = 'nJSclvl9V2lsof5b7URGHrdEVzMeMvbQ7tYpT57w'
-const API_DATE = '2023-02-28'
+const API_KEY = '1Pj3D3xLuzlgf4xeuzJk28OfHUqQwe0LksLWgJLN'
+let API_DATE = '2023-02-28'
 const rootElement = document.querySelector('#root')
 
 const fetchApod = async function(url) {
@@ -8,74 +8,49 @@ const fetchApod = async function(url) {
   return apodData
 }
 
-const fetchApod2 = async function(url) {
-  return fetch(url)
-          .then((response) => response.json())
-          //.then((apodData) => apodData)
+// explanation, date, eventListener - dinamikus
+// statikus soha nem lesz cserélve (dátumválasztó)
+const createSkeleton = function() {
+  const div1 = document.createElement('div')
+  div1.classList.add('input-container')
+
+  const div2 = document.createElement('div')
+  div2.classList.add('data-container')
+  rootElement.append(div1, div2)
 }
 
-const createApodElement = function(title, url) {
+const createApodElement = function(parentElement, data) {
+  //parentElement.innerHTML = '' (az elem önmagát fűzi hozzá) 
   const h1 = document.createElement('h1')
-  h1.innerText = title
+  h1.innerText = data.title
 
   const img = document.createElement('img')
-  img.src = url
+  img.src = data.url
 
-  rootElement.append(h1, img)
+  parentElement.append(h1, img)
 }
 
 const init = async function() {
+  createSkeleton()
+  const inputContainer = document.querySelector('.input-container')
+  const dataContainer = document.querySelector('.data-container')
   const apodData = await fetchApod(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${API_DATE}`)
-  createApodElement(apodData.title, apodData.url)
+  console.log(apodData)
+  createApodElement(dataContainer, apodData)
+  
 }
 
 init()
 
-// const API_KEY = '1Pj3D3xLuzlgf4xeuzJk28OfHUqQwe0LksLWgJLN'
-// const API_DATE = '2023-02-28'
-// const root = document.querySelector('#root')
+// function createDog (name, age, color) { 
+//   // const dog = 'morzsa'
+//   // const age = 2
+//   const h2 = document.createElement('h2')
+//   h2.classList.add('dog')
+//   h2.innerText = `${name}, age: ${age}, color: ${color}`
+//   rootElement.append(h2)
 
-//   async function getFetch(url) {
-//   const response = await fetch(url)
-//   const apodData = await response.json() 
-  
-//   return apodData
 // }
-// console.log(
-//   getFetch(
-//     `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${API_DATE}`
-//   )
-// )
+// createDog('Artúr', 1, 'barna')
 
-// async function fetch2(url) {
-//   return fetch(url)
-//     .then((response) => response.json())
-//   //.then((apodData) => apodData)
-// }
-
-//   function getElements() {
-//     let a = async function() {
-//     const response = await fetch2 (
-//       `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${API_DATE}`
-//     )
-
-//   const h1 = document.createElement('h1')
-//   h1.innerText = title
-
-//   const p = document.createElement ('p')
-
-//   const img = document.createElement('img')
-//   img.src = url
-
-//   root.append(h1, p, img)
-//   a()
-//   }
-// }
-// getElements()
-
-// // const init = async function() {
-// //   const apodData = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${API_DATE}`)
-// //   createElement(h1.title, img.url, p)
-// // }
-
-// // init()
+// createDog('Süti', 4, 'fehér')
